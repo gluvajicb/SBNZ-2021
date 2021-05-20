b8603ab0dcd1255a29eb13736a4445e74eedccde
mihailob <mihabubnjevic@gmail.com>
2021-05-21 00:56:04 +0200

    [API] small fix model

--- api/src/main/java/com/example/demo/Model/BuildRecommendSession.java
+++ api/src/main/java/com/example/demo/Model/BuildRecommendSession.java
@@ -1,4 +1,23 @@
 package com.example.demo.Model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BuildRecommendSession {
+
+    private Item startingItem;
+
+    private Item mythicItem;
+
+    private Item boots;
+
+    private Item counterItem;
+
+    private Item situationalItem1;
+
+    private Item situationalItem2;
 }
