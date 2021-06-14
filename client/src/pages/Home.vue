b703fe22b20a1f8a6f2ff93b84da626d98dee2ed
Branko <branko.gluvajic@marbleit.rs>
2021-06-14 14:08:30 +0200

    [Client] Home page added

---
+++ client/src/pages/Home.vue
@@ -0,0 +1,93 @@
+<template>
+        <div class="main">
+            <div class="wrapper">
+
+                <div class="champ item">
+                    <h1 class="font">Champions</h1>
+                </div>
+
+                <div class="build item">
+                    <h1 class="font">Item Build</h1>
+                </div>
+
+            </div>
+        </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'Home',
+    }
+
+</script>
+
+<style>
+
+.item{
+  width: 100%;
+  max-width: 350px;
+  height: 600px;
+  background-color: red;
+  margin: 0 100px;
+  transition: ease-in-out 0.3s;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+}
+
+.champ {
+  background-image: url("../assets/katarina.jpg");
+  background-position: -160px;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.build {
+  background-image: url("../assets/items.jpg");
+  background-position: 62.5%;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.item:hover {
+  transform: scale(1.2);
+  transition: ease-in-out 0.3s;
+  cursor: pointer;
+}
+
+.font {
+  color: white;
+  font-size: 40px;
+}
+
+.item_wrap {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.main {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex; 
+  flex-direction: column;
+  display: flex;
+  justify-content: space-around;
+  width: 100vw;
+  height: 100vh;
+}
+
+</style>
--- client/src/router.js
+++ client/src/router.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Home from '@/pages/Home'
+import Champions from '@/pages/Champions'
+
+
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
     
     routes: [
-        
+        {
+            path: '/home',
+            component: Home
+        },
+        {
+            path: 'champions',
+            component: Champions
+        }
     ]
 })
